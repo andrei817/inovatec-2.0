2c89a6e9e42d4e8d0a67769146ef682ab83bab42
andrei817 <andreieluiz234@gmail.com>
2024-12-03 01:42:13 -0300

    Update lista de staff.php

--- lista de staff.php
+++ lista de staff.php
@@ -36,6 +36,7 @@ $totalPaginas = ceil($totalStaffs / $staffsPorPagina);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="ico/SGE.ico" type="image/x-icon">
     <link rel="stylesheet" href="lista de staff.css">
     <title>Staffs de Eventos</title>
     
