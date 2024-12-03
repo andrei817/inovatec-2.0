aa9633f3e7ce39e805c109ee9a4501b11d339c4a
andrei817 <andreieluiz234@gmail.com>
2024-12-03 01:39:38 -0300

    Update perfil do produtor.php

--- perfil do produtor.php
+++ perfil do produtor.php
@@ -57,6 +57,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="ico/SGE.ico" type="image/x-icon">
     <link rel="stylesheet" href="perfil do produtor.css">
     <title>Editar Perfil</title>
 </head>
