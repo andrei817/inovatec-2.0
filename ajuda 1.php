4d36679f4692cfa458b20dfcfdcd936b2c500ba0
andrei817 <andreieluiz234@gmail.com>
2024-12-04 05:17:45 -0300

    Update ajuda 1.php

--- ajuda 1.php
+++ ajuda 1.php
@@ -65,6 +65,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['email']) && isset($_PO
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="ico/SGE.ico" type="image/x-icon">
     <title>SGE - Ajuda</title>
     <link rel="stylesheet" href="ajuda.css">
     
@@ -90,12 +91,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['email']) && isset($_PO
    </div>
         <nav>
             <ul>   
-                <li>  <a href="index.php"> Home</a></li> 
-                <li> <a href= "ajuda 1.php">Ajuda</a></li>
-                <li> <a href="Sobre 1.php"> Sobre </a></li>
-                 <li>  <a onclick="abrirPopUp()"> <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
-                  <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
-                </svg></a></li>
+        <li><a href="index.php" title="Página inicial">Home</a></li>  
+        <li><a href="ajuda 1.php" title="Obtenha ajuda">Ajuda</a></li>
+        <li><a href="Sobre 1.php" title="Sobre nós">Sobre</a></li>
+        <li><a onclick="abrirPopUp()" title="Área Restrita">
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
+        <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+    </svg>
+</a></li>
 
 <!-- Overlay e Popup -->
 <div id="overlay">
@@ -227,9 +230,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['email']) && isset($_PO
        
         <a href="index.php" class="close-btn-ajuda">&times;</a>
         <div class="help-container">
+
+        <div class="ajuda-title">
             <h1 class="Ajuda"> Ajuda</h1>
             <p> Olá seja bem vindo ao S.G.E. aqui a baixo estão as opções de ajuda caso você precise compreender melhor nosso site</p>
-            
+        </div>
             <div class="help-section">
               <button class="help-title" onclick="toggleHelp(this)">Agenda</button>
               <div class="help-content">
@@ -269,6 +274,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['email']) && isset($_PO
         </div>
           </div> 
    </section>
+
     <script src="ajuda.js"> </script>
 </body>
 </html>
