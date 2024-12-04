f3e0c324e194ff164c0b8fd8428e1759fffc0196
andrei817 <andreieluiz234@gmail.com>
2024-12-04 05:10:48 -0300

    Update lista de buffet.php

--- lista de buffet.php
+++ lista de buffet.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="ico/SGE.ico" type="image/x-icon">
     <link rel="stylesheet" href="lista de buffet.css">
     <title>Lista de Buffets</title>
 </head>
@@ -24,8 +25,7 @@
 </div>
 
 <script> 
-// Função para abrir a sidebar
-function abrirSidebar() {
+    function abrirSidebar() {
     if (window.innerWidth <= 768) {
       document.getElementById("mySidebar").style.width = "100%";
     } else {
@@ -139,14 +139,19 @@ $totalPaginas = ceil($totalBuffets / $buffetsPorPagina);
                 <td><?php echo $buffet['nome']; ?></td>
                 <td><?php echo $buffet['descricao']; ?></td>
                 <td class="actions">
-                <a class="a" href="edit buffet.php?id=<?php echo $buffet['id']; ?>"> <button class='btn-edit'> <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>
+                <a class="a" href="edit buffet.php?id=<?php echo $buffet['id']; ?>"> <button class='btn-edit' title="Editar"> <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>
 <path d='M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z'/>
 <path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z'/> </a> </svg> </button>
 
 
-<a class="a" href="delete Buffet.php?delete=<?php echo $buffet['id']; ?>"><button class='btn-delete' > <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-<path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
-</svg> </a> </button>
+<a class="a" href="#" onclick="abrirModal(<?php echo $buffet['id']; ?>)">
+  <button class='btn-delete' title="Deletar">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+      <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+    </svg>
+  </button>
+</a>
+
 
                 </td>
             </tr>
@@ -154,6 +159,41 @@ $totalPaginas = ceil($totalBuffets / $buffetsPorPagina);
     </tbody>    
 
 </table>
+
+<!-- Modal de Confirmação -->
+<div id="confirmModal" class="modal-delete">
+  <div class="modal-content-delete">
+    <h2>Tem certeza de que deseja excluir este buffet?</h2>
+    <div class="modal-buttons">
+      <button onclick="confirmarExclusao()" class="btn-confirm">Sim</button>
+      <button onclick="fecharModal()" class="btn-cancel">Não</button>
+    </div>
+  </div>
+</div>
+
+
+<script> 
+let buffetIdToDelete; // Variável para armazenar o ID do buffet a ser excluído
+
+// Função para abrir o modal e armazenar o ID
+function abrirModal(id) {
+  buffetIdToDelete = id; // Armazena o ID do buffet selecionado
+  document.getElementById('confirmModal').style.display = 'flex'; // Exibe o modal
+}
+
+// Função para fechar o modal
+function fecharModal() {
+  document.getElementById('confirmModal').style.display = 'none'; // Oculta o modal
+}
+
+// Função para confirmar a exclusão
+function confirmarExclusao() {
+  // Redireciona para o script de exclusão com o ID armazenado
+  window.location.href = 'delete Buffet.php?delete=' + buffetIdToDelete;
+}
+
+</script>
+
  <!-- Links de Paginação -->
  <div class="pagination">
             <?php for ($i = 1; $i <= $totalPaginas; $i++): ?>
