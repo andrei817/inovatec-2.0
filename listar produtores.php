34979d63c75f25861374e655907ba5f21792bbd9
andrei817 <andreieluiz234@gmail.com>
2024-12-05 00:20:06 -0300

    Update listar produtores.php

--- listar produtores.php
+++ listar produtores.php
@@ -144,7 +144,7 @@ $result = mysqli_query($conn, $sql);
   <path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z'/> </a> </svg> </button>
 
 
- <a class="a" title="Deletar" href="delete prod.php?id=<?php echo $produtor['id']; ?>" onclick="return confirm('Tem certeza que deseja excluir este produtor?');">  <button class='btn-delete'> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+  <a class="a" title="Deletar" href="#" onclick="openModal('delete prod.php?id=<?php echo $produtor['id']; ?>'); return false;">  <button class='btn-delete'> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
   <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
 </svg> </a> </button>
 
@@ -155,6 +155,38 @@ $result = mysqli_query($conn, $sql);
             </tbody>
         </table>
 
+        <!-- Modal de Confirmação -->
+<div id="confirmModal" class="modal-delete">
+    <div class="modal-content-delete">
+        <p>Deseja realmente excluir este produtor?</p>
+        <div class="modal-buttons">
+            <button class="btn-confirm" onclick="confirmDelete()">Sim</button>
+            <button class="btn-cancel" onclick="closeModalDelete()">Não</button>
+        </div>
+    </div>
+</div>
+
+<script>
+let deleteUrl = ""; // Variável para armazenar o URL
+
+function openModal(url) {
+    deleteUrl = url; // Guarda o URL do botão clicado
+    document.getElementById('confirmModal').style.display = 'flex'; // Mostra o modal
+}
+
+// Função chamada quando o usuário confirma a exclusão
+function confirmDelete() {
+    window.location.href = deleteUrl; // Redireciona para o link de exclusão
+}
+
+// Função para fechar o modal sem excluir
+function closeModalDelete() {
+    document.getElementById('confirmModal').style.display = 'none';
+}
+</script>
+
+
+
         <!-- Navegação da Paginação -->
         <div class="pagination">
             <?php for ($i = 1; $i <= $total_paginas; $i++): ?>
@@ -164,7 +196,6 @@ $result = mysqli_query($conn, $sql);
             <?php endfor; ?>
         </div>
 
-        
     <?php else: ?>
         <p>Nenhum produtor de eventos cadastrado.</p>
     <?php endif; ?>
